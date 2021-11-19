9b50769f1adff1b1bd988ee6369f71cabc963dff
Cosha777 <artemiy2007@ukr.net>
2021-11-19 18:45:36 +0200

    second

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.gittest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        textView = findViewById(R.id.text);
+
+        textView.setText("Second");
+
+
+
     }
 }
